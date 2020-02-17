a4605d16348c3d82fc1a295104672613662b2392
Samigullin Nikita <nikita.samigullin@travisperkins.co.uk>
2020-02-17 18:09:58 +0200

    Control work 1

---
+++ src/test/java/JadenTextTests.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JadenTextTests {
+
+    JadenCase jadenCase = new JadenCase();
+
+    @Test
+    public void test() {
+        assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", jadenCase.toJadenCase("most trees are blue"), "Most Trees Are Blue");
+    }
+
+    @Test
+    public void testNullArg() {
+        assertNull("Must return null when the arg is null", jadenCase.toJadenCase(null));
+    }
+
+    @Test
+    public void testEmptyArg() {
+        assertNull("Must return null when the arg is null", jadenCase.toJadenCase(""));
+    }
+}
