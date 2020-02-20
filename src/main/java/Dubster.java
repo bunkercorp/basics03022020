3ebbe19ed3c7ee985ed478bb9663d15edb38222a
SanQAOdessa <insane@online.ua>
2020-02-20 08:54:01 +0200

    dubste add control size input

--- src/main/java/Dubster.java
+++ src/main/java/Dubster.java
@@ -26,6 +26,10 @@ Return the words of the initial song that Polycarpus used to make a dubsteb remi
 
 public class Dubster {
     public static String songDecoder(String song) {
+        if(song == "" || song == null)  return "";
+        if(!song.equals(song.toUpperCase())) return "";
+        if(song.length()>200) return "";
+
         boolean flagSp=false;
         String tempStr = "";
         for(int i = 0; i< song.length()-2;i++){
