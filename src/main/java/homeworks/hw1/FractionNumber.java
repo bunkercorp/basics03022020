3c9621d6d383ab9970b50bd80860cee47a289276
DimaBoyev <dmitriy.boyev@gmail.com>
2020-03-01 12:47:28 +0200

    String formats updated in method toString

--- src/main/java/homeworks/hw1/FractionNumber.java
+++ src/main/java/homeworks/hw1/FractionNumber.java
@@ -55,22 +55,20 @@ public class FractionNumber {
     public String toString() {
         String fraction = "";
         if (FractionNumber.this.numerator < FractionNumber.this.denominator && FractionNumber.this.numerator > 0) {
-            fraction = numerator + "/" + denominator;
+            fraction = String.format("%d%s%d", numerator, "/", denominator);
         } else if (FractionNumber.this.numerator % FractionNumber.this.denominator == 0) {
             long result = numerator / denominator;
             fraction = "" + result;
         } else if (FractionNumber.this.numerator > FractionNumber.this.denominator && FractionNumber.this.numerator % FractionNumber.this.denominator != 0) {
             long result = numerator / denominator;
             long result2 = numerator % denominator;
-            fraction = result + " " + result2 + "/" + denominator;
+            fraction = String.format("%d%s%d%s%d", result, " ", result2, "/", denominator);
         } else if (FractionNumber.this.numerator < 0 && Math.abs(FractionNumber.this.numerator) < FractionNumber.this.denominator) {
-            long result = Math.abs(numerator / denominator);
-            long result2 = Math.abs(numerator % denominator);
-            fraction = numerator + "/" + denominator;
+            fraction = String.format("%d%s%d", numerator, "/", denominator);
         } else if (FractionNumber.this.numerator < FractionNumber.this.denominator && FractionNumber.this.numerator % FractionNumber.this.denominator != 0) {
             long result = Math.abs(numerator / denominator);
             long result2 = Math.abs(numerator % denominator);
-            fraction = "-" + result + " " + result2 + "/" + denominator;
+            fraction = String.format("%s%d%s%d%s%d", "-", result, " ", result2, "/",denominator);
         }
 
         return fraction;
