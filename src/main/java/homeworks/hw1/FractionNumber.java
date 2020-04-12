431e5ea36c111985d186df9ee95ca01dfccde8f2
Victoria Antonova <antonovava@gmail.com>
2020-04-12 16:44:22 +0300

    cw6

---
+++ src/main/java/homeworks/hw1/FractionNumber.java
@@ -0,0 +1,63 @@
+package homeworks.hw1;
+
+public class FractionNumber {
+    private long num;
+    private long den;
+
+    public FractionNumber(long num, long den) {
+        if (den == 0) {
+            throw new RuntimeException("Denominator equals to zero");
+        }
+        if (den < 0) {
+            num = -num;
+            den = -den;
+        }
+        this.num = num;
+        this.den = den;
+        reduce();
+    }
+
+    private long calculate(long num, long den) {
+        if (num % den == 0) {
+            return den;
+        }
+        return calculate(den, num % den);
+    }
+
+    private void reduce() {
+        long calculate = calculate(num, den);
+        num /= calculate;
+        den /= calculate;
+    }
+
+    public FractionNumber add(FractionNumber r) {
+        return new FractionNumber(this.num * r.den + r.num * this.den, this.den * r.den);
+    }
+
+    public FractionNumber subtract(FractionNumber r) {
+        return new FractionNumber(this.num * r.den - r.num * this.den, this.den * r.den);
+    }
+
+    public FractionNumber multiply(FractionNumber r) {
+        return new FractionNumber(this.num * r.num, this.den * r.den);
+    }
+
+    public FractionNumber divide(FractionNumber r) {
+        return new FractionNumber(this.num * r.den, this.den * r.num);
+    }
+
+    @Override
+    public String toString() {
+        String fraction = "";
+        if (FractionNumber.this.num < FractionNumber.this.den && FractionNumber.this.num > 0) {
+            fraction = String.format("%d/%d", num, den);
+        } else if (FractionNumber.this.num % FractionNumber.this.den == 0) {
+            fraction = String.format("%d",num / den);
+        } else if (FractionNumber.this.num > FractionNumber.this.den && FractionNumber.this.num % FractionNumber.this.den != 0) {
+            fraction = String.format("%d %d/%d", num / den, num % den, den);
+        } else if (FractionNumber.this.num < 0 && Math.abs(FractionNumber.this.num) < FractionNumber.this.den) {
+            fraction = String.format("%d/%d", num, den);
+        }
+        return fraction;
+    }
+}
