85cffa4654a662f90d9142fc4f103988b832cfd8
Samigullin Nikita <nikita.samigullin@travisperkins.co.uk>
2020-04-20 19:34:29 +0300

    Added comments

--- src/main/java/homeworks/hw1/FractionNumber.java
+++ src/main/java/homeworks/hw1/FractionNumber.java
@@ -8,10 +8,12 @@ public class FractionNumber {
     public FractionNumber(long a, long b) {
         this.numerator = a;
         this.denominator = b;
+        // этот иф стоило бы поставить вотпрям в самое начало, так как при этой ситуации не нужно никаких присваиваний, что у тебя вверху
+        // на быстродействии не особо скажется, но чистота формы будет выше
         if (this.denominator == 0) {
             throw new RuntimeException("Denominator equals to zero");
         }
-
+        // мне тут даже идея подсвечивает что этот иф не нужен, все сработает и так, так как если верхний иф сработает, то сюда не доходим, а если нет  - то доходим
         if (this.denominator != 0) {
             long gcd = Math.abs(reduce(this.numerator, this.denominator));
             System.out.println(this.numerator + " - " + this.denominator);
@@ -24,6 +26,7 @@ public class FractionNumber {
     }
 
     public FractionNumber add(FractionNumber fraction) {
+      // а толку с этих присваиваний?
         long a1 = this.numerator;
         long a2 = this.denominator;
         long b1 = fraction.numerator;
@@ -31,7 +34,7 @@ public class FractionNumber {
         System.out.println(a1 + " - " + b1+"  "+a2+" - "+b2);
         return new FractionNumber(((a1 * b2) + (a2 * b1)), (a2 * b2));
     }
-
+    // тут полшага не хватает ,чтобы общую для add \ subtract логику вывести в приватный метод
     public FractionNumber subtract(FractionNumber fraction) {
         long a1 = this.numerator;
         long a2 = this.denominator;
@@ -55,6 +58,7 @@ public class FractionNumber {
         long b1 = fraction.numerator;
         long b2 = fraction.denominator;
         System.out.println(a1 + "- " + b1+"  "+a2+" - "+b2+"   "+(a1 * b2));
+        //(a1 * b2), (a2 * b1) просятся в отдельные переменные
         if ( (a2 * b1) < 0) {
             return new FractionNumber(-1 * (a1 * b2), (a2 * b1));
         } else {
@@ -70,6 +74,7 @@ public class FractionNumber {
         } else if (Math.abs(this.numerator) > Math.abs(this.denominator)) {
             long intpart = this.numerator / this.denominator;
             long fraction = Math.abs(this.numerator % this.denominator);
+            // String.format . Просто оставлю здесь ;)
             return intpart + " " + fraction + "/" + this.denominator;
         } else return this.numerator + " / " + this.denominator;
     }
