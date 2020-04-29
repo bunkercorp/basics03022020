8abd248a770a5c655940c12a1f02c834823cb258
Catharine852 <catharina.afanasyeva@gmail.com>
2020-04-29 22:34:49 +0300

    a little bit more

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,7 @@
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,6 +10,8 @@ import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class FirstTest {
@@ -31,11 +35,49 @@ public class FirstTest {
         wdWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"aui-flag-container\"]/div/div"))).getText();
         Assert.assertEquals("AQA220-4 has been assigned.", wdWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"aui-flag-container\"]/div/div"))).getText());
 
-
-
-        Thread.sleep(5000);
-        browser.quit();
+//        Thread.sleep(5000);
+//        browser.quit();
     }
+
+    @Test
+    public void testBasic2() throws InterruptedException {
+        WebDriver browser = giveMeBrowser("chrome");
+        browser.get("https://jira.hillel.it/browse/AQA220-4");
+        WebElement username = browser.findElement(By.id("login-form-username"));
+        WebElement password = browser.findElement(By.id("login-form-password"));
+        username.sendKeys(System.getProperty("username"));
+        password.sendKeys(System.getProperty("password"));
+        browser.findElement(By.xpath("//*[@id=\"login-form-submit\"]")).click();
+        WebDriverWait wdWait = new WebDriverWait(browser,10);
+
+        WebElement priority = wdWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("priority-val")));
+        priority.click();
+        List<WebElement> listPrio = wdWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("option.imagebacked")));
+        for (int i = 0; i < listPrio.size(); i++) {
+            System.out.println(listPrio.get(i).getAttribute("label"));
+
+        }
+        browser.findElement(By.cssSelector("strong.name")).click();
+
+        int random = (int) (Math.random()*10 + 5);
+        String generatedLabel = RandomStringUtils.randomAlphanumeric(random);
+        browser.findElement(By.id("labels-72352-value")).click();
+        wdWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("labels-textarea"))).sendKeys(generatedLabel, Keys.ENTER);
+        wdWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.aui-button.submit"))).click();
+
+        browser.findElement(By.id("description-val")).click();
+        wdWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("mce_0_ifr")));
+        browser.switchTo().frame("mce_0_ifr");
+        WebElement Description = browser.findElement(By.xpath("//*[@id=\"tinymce\"]/p"));
+        Description.sendKeys("Their saved linen downs tears son add music");
+        browser.switchTo().defaultContent();
+        browser.findElement(By.cssSelector("button.aui-button.aui-button-primary.submit")).click();
+
+
+//        Thread.sleep(5000);
+//        browser.quit();
+    }
+
     static WebDriver giveMeBrowser(String browserName) throws InterruptedException {
         WebDriver browser;
         switch (browserName.toLowerCase().trim()) {
