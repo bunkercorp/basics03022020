5b8d2d83a962bcfbe6e5404244181ef203e84afb
Samigullin Nikita <nikita.samigullin@travisperkins.co.uk>
2020-05-28 18:39:54 +0300

    Test 1

--- src/test/java/Victim.java
+++ src/test/java/Victim.java
@@ -13,9 +13,6 @@ public class Victim{
         System.out.println("Test 1 ");
     }
 
-
-
-
     @Test
     public static void test2() throws MalformedURLException, InterruptedException {
         System.out.println("Test 2");
