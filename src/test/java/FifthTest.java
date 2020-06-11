4ca70b6c908699bb00a8bc13899a0dd947a6e37c
Igor Shapovalov <shapovalovii2000@gmail.com>
2020-06-11 19:43:12 +0300

    create 5 tests

---
+++ src/test/java/FifthTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class FifthTest {
+    private WebDriver driver;
+    private WebDriverWait waitElement;
+
+    private By inputGoogle = By.xpath("//input[@name='q']");
+
+    @Test
+    public void testBasic() {
+        final String binPath = String.format("%s/bin/chromedriver.exe", System.getProperty("user.dir"));
+        System.setProperty("webdriver.chrome.driver", binPath);
+
+        driver = new ChromeDriver();
+        waitElement = new WebDriverWait(driver, 10);
+
+        driver.get("https://www.google.com/");
+
+        waitElement.until(ExpectedConditions.presenceOfElementLocated(inputGoogle));
+
+        driver.findElement(inputGoogle).sendKeys("123456789");
+
+        //driver.quit();
+    }
+}
